6b0519d55a3c781508ba6c1d99ac7d6545b361f8
helochen <3334972898@qq.com>
2017-08-21 23:55:15 +0800

    测试spring bean

---
+++ learn21/src/main/java/com/learn/two/MyTestBean.java
@@ -0,0 +1,15 @@
+package com.learn.two;
+/**
+ * Created by Chen on 2017/8/21.
+ */
+public class MyTestBean {
+    private String testStr = "testStr";
+
+    public String getTestStr() {
+        return testStr;
+    }
+
+    public void setTestStr(String testStr) {
+        this.testStr = testStr;
+    }
+}
